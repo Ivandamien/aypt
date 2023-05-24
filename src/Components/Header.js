791bac396942dc2dc6e8a83e2fa664c21262b730
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-05-24 15:45:24 +0300

    importing assets and fixing links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from 'react';
+import { IoSearchOutline, IoCartOutline, IoArrowForwardOutline, IoMenuOutline, IoCloseOutline } from 'react-icons/io5';
+import { logo } from '../assets/images';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const toggleMenu = () => {
+    // Handle menu toggle logic
+  };
 
-export default Header
+  const toggleSearch = () => {
+    // Handle search toggle logic
+  };
+
+  const openCart = () => {
+    // Handle cart opening logic
+  };
+
+  return (
+    <header className="header" data-header>
+      <div className="container">
+        <Link to={'/'} className="logo">
+          <img src={logo} width="162" height="50" alt="EduWeb logo" />
+        </Link>
+
+        <nav className="navbar" data-navbar>
+          <div className="wrapper">
+            <Link to={'/'} className="logo">
+              <img src={logo} width="162" height="50" alt="EduWeb logo" />
+            </Link>
+
+            <button className="nav-close-btn" aria-label="close menu" onClick={toggleMenu}>
+              <IoCloseOutline aria-hidden="true" />
+            </button>
+          </div>
+
+          <ul className="navbar-list">
+            <li className="navbar-item">
+              <Link to={'/'} className="navbar-link" data-nav-link>
+                Home
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to={'/'} className="navbar-link" data-nav-link>
+                About
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to={'/'} className="navbar-link" data-nav-link>
+                Courses
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to={'/'} className="navbar-link" data-nav-link>
+                Blog
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to={'/'} className="navbar-link" data-nav-link>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="header-actions">
+          <button className="header-action-btn" aria-label="toggle search" title="Search" onClick={toggleSearch}>
+            <IoSearchOutline aria-hidden="true" />
+          </button>
+          <button className="header-action-btn" aria-label="cart" title="Cart" onClick={openCart}>
+            <IoCartOutline aria-hidden="true" />
+            <span className="btn-badge">0</span>
+          </button>
+          <Link to={'#'} className="btn has-before">
+            <span className="span">Try for free</span>
+            <IoArrowForwardOutline aria-hidden="true" />
+          </Link>
+          <button className="header-action-btn" aria-label="open menu" data-nav-toggler onClick={toggleMenu}>
+            <IoMenuOutline aria-hidden="true" />
+          </button>
+        </div>
+
+        <div className="overlay" data-nav-toggler data-overlay></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/assets/images/index.js
@@ -0,0 +1,60 @@
+import aboutBanner from './about-banner.jpg';
+import aboutShape1 from './about-shape-1.svg';
+import aboutShape2 from './about-shape-2.svg';
+import aboutShape3 from './about-shape-3.png';
+import aboutShape4 from './about-shape-4.svg';
+import blog1 from './blog-1.jpg';
+import blog2 from './blog-2.jpg';
+import blog3 from './blog-3.jpg';
+import blogBg from './blog-bg.svg';
+import blogShape from './blog-shape.png';
+import category1 from './category-1.svg';
+import category2 from './category-2.svg';
+import category3 from './category-3.svg';
+import course1 from './course-1.jpg';
+import course2 from './course-2.jpg';
+import course3 from './course-3.jpg';
+import footerBg from './footer-bg.png';
+import heroBanner1 from './hero-banner-1.jpg';
+import heroBanner2 from './hero-banner-2.jpg';
+import heroBg from './hero-bg.svg';
+import heroShape1 from './hero-shape-1.svg';
+import heroShape2 from './hero-shape-2.png';
+import logoLight from './logo-light.svg';
+import logo from './logo.svg';
+import videoBanner from './video-banner.jpg';
+import videoBg from './video-bg.png';
+import videoShape1 from './video-shape-1.png';
+import videoShape2 from './video-shape-2.png';
+
+export { 
+    aboutBanner,
+    aboutShape1,
+    aboutShape2,
+    aboutShape3,
+    aboutShape4,
+    blog1,
+    blog2,
+    blog3,
+    blogBg,
+    blogShape,
+    category1,
+    category2,
+    category3,
+    course1,
+    course2,
+    course3,
+    footerBg,
+    heroBanner1,
+    heroBanner2,
+    heroBg,
+    heroShape1,
+    heroShape2,
+    logoLight,
+    logo,
+    videoBanner,
+    videoBg,
+    videoShape1,
+    videoShape2
+    
+}
