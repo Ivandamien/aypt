01460ec7e5211620c1a8f9d9901fc087b29c0580
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-05-24 16:12:52 +0300

    Making the navbar responsive

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoSearchOutline, IoCartOutline, IoArrowForwardOutline, IoMenuOutline, IoCloseOutline } from 'react-icons/io5';
-import { logo } from '../assets/images';
 import { Link } from 'react-router-dom';
-
+import { logo } from '../assets/images';
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   const toggleMenu = () => {
-    // Handle menu toggle logic
+    setMenuOpen(!isMenuOpen);
   };
 
   const toggleSearch = () => {
@@ -17,6 +18,10 @@ const Header = () => {
     // Handle cart opening logic
   };
 
+  const closeNavbar = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header className="header" data-header>
       <div className="container">
@@ -24,7 +29,7 @@ const Header = () => {
           <img src={logo} width="162" height="50" alt="EduWeb logo" />
         </Link>
 
-        <nav className="navbar" data-navbar>
+        <nav className={`navbar ${isMenuOpen ? 'active' : ''}`} data-navbar>
           <div className="wrapper">
             <Link to={'/'} className="logo">
               <img src={logo} width="162" height="50" alt="EduWeb logo" />
@@ -37,27 +42,27 @@ const Header = () => {
 
           <ul className="navbar-list">
             <li className="navbar-item">
-              <Link to={'/'} className="navbar-link" data-nav-link>
+              <Link to={'/'} className="navbar-link" data-nav-link onClick={closeNavbar}>
                 Home
               </Link>
             </li>
             <li className="navbar-item">
-              <Link to={'/'} className="navbar-link" data-nav-link>
+              <Link to={'/'} className="navbar-link" data-nav-link onClick={closeNavbar}>
                 About
               </Link>
             </li>
             <li className="navbar-item">
-              <Link to={'/'} className="navbar-link" data-nav-link>
+              <Link to={'/'} className="navbar-link" data-nav-link onClick={closeNavbar}>
                 Courses
               </Link>
             </li>
             <li className="navbar-item">
-              <Link to={'/'} className="navbar-link" data-nav-link>
+              <Link to={'/'} className="navbar-link" data-nav-link onClick={closeNavbar}>
                 Blog
               </Link>
             </li>
             <li className="navbar-item">
-              <Link to={'/'} className="navbar-link" data-nav-link>
+              <Link to={'/'} className="navbar-link" data-nav-link onClick={closeNavbar}>
                 Contact
               </Link>
             </li>
@@ -81,7 +86,7 @@ const Header = () => {
           </button>
         </div>
 
-        <div className="overlay" data-nav-toggler data-overlay></div>
+        {isMenuOpen && <div className="overlay" data-nav-toggler data-overlay onClick={closeNavbar}></div>}
       </div>
     </header>
   );
