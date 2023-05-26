1451fd91b5aa2f5b25d9179e7d3ae8bc627bf606
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-05-26 17:10:26 +0300

    Final touches

--- src/Components/Name.js
+++ src/Components/Name.js
@@ -3,10 +3,15 @@ import './Name.css';
 
 const Name = ({ width, height, className }) => {
   return (
-    <div className={`name-container ${className}`} style={{ width, height }}>
-      <div className="progress-bar"></div>
-      <h1 className="name-title">Ray Sanchez</h1>
-      <p className="name-text">Lorem10</p>
+      <div className={`name-container ${className}`} style={{ width, height }}>
+          <div className="outer-bar">
+            <div className="progress-bar">
+                <div className="progress" style={{ transform: 'rotate(270deg)' }}></div>
+            </div>
+              
+          </div>
+      <h1 className="name-title">We Build the future</h1>
+      <p className="name-text"></p>
     </div>
   );
 };
